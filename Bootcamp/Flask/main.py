71b1c529392250daaf151bc428433d29fa8b86f5
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2024-07-09 00:58:01 -0400

    Initial commit

---
+++ Bootcamp/Flask/main.py
@@ -0,0 +1,6 @@
+# Flask is a framework
+# Framework is a set of instruments that help developers create producrts 
+# such as sites, apps, etc.
+# Framework Flask creates web apps based on Python, HTML, CSS, JavaScript
+
+print('Hello world');
