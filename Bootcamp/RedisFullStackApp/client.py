5eb188b8a759c5be283c0ea716934a03523fcdbf
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2024-09-28 18:24:45 -0400

    added client and server working with redis

--- Bootcamp/RedisFullStackApp/client.py
+++ Bootcamp/RedisFullStackApp/client.py
@@ -1,6 +1,11 @@
 import redis
 
 with redis.Redis() as redis_client:
-    value = redis_client.rpop("queue")  # rpop убирает элемент справа (FirstInFirstOut)
+    value = redis_client.rpop("queue")  # rpop removes and reutrns value on the right (FirstInFirstOut)
 
 print(int(value))
+
+# with redis.Redis() as redis_client:
+#     value = redis_client.brpop("queue")  # brpop waits for a new value if the list is empty
+
+# print(int(value[1]))  # value[0] will be the array's name in this case
