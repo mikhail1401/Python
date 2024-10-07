d484352622b6a96e73cb8d29c03a0244fa63cbb3
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2024-10-07 01:01:22 -0400

    Created a web abb based on Flask (Flask2)

--- Bootcamp/Flask2/main.py
+++ Bootcamp/Flask2/main.py
@@ -1,10 +1,29 @@
-from flask import Flask
+# This lesson is about HTML, CSS, Jinja
+# Jinja - шаблонизатор, позволяющий вставлять в html динамические данные (templating engine)
+# php - это язык, который отвечает за перепроцессор html
 
-app = Flask(__name__)   # __name__ holds the name of the current module (main.py)
+
+from flask import Flask, render_template
+
+# the app is our server
+app = Flask(__name__)   # in python __name__ holds the name of the current module (main.py)
 
 @app.route('/')
 def main():
-    return ''
+    names = ["Алекскей", "Михаил Ф.", "София",
+             "Михаил А.", "Алиса", "Кристина Н",
+             "Кристина Х.", "Маруся", "Елена",
+             "Дмитрий", "Ярослав М.", "Ярослав А."]
+    return render_template('base.html', title='Main Page', names=names)
+
+@app.route('/students')
+def students():
+    names = ["Алекскей", "Михаил Ф.", "София",
+             "Михаил А.", "Алиса", "Кристина Н",
+             "Кристина Х.", "Маруся", "Елена",
+             "Дмитрий", "Ярослав М.", "Ярослав А."]
+    return render_template('students.html', title='Список всех студентов',
+                           names=names)
 
 if __name__ == '__main__':
     app.run()
