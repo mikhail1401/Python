62a95d02e91f661d81647719b09544797c97c3b0
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2024-10-14 23:43:05 -0400

    Added test_super.html to Flask2 and information more jinja functionalitites

--- Bootcamp/Flask2/main.py
+++ Bootcamp/Flask2/main.py
@@ -1,5 +1,5 @@
 # This lesson is about HTML, CSS, Jinja
-# Jinja - шаблонизатор, позволяющий вставлять в html динамические данные (templating engine)
+# Jinja - шаблонизатор (templating engine), позволяющий вставлять в html динамические данные
 # php - это язык, который отвечает за перепроцессор html
 
 
@@ -10,11 +10,11 @@ app = Flask(__name__)   # in python __name__ holds the name of the current modul
 
 @app.route('/')
 def main():
-    names = ["Алекскей", "Михаил Ф.", "София",
-             "Михаил А.", "Алиса", "Кристина Н",
-             "Кристина Х.", "Маруся", "Елена",
-             "Дмитрий", "Ярослав М.", "Ярослав А."]
-    return render_template('base.html', title='Main Page', names=names)
+    context = {
+        "title": "Главная страница",
+        "author_name": "Mikhail"
+    }
+    return render_template('base.html', **context)
 
 @app.route('/students')
 def students():
@@ -24,6 +24,11 @@ def students():
              "Дмитрий", "Ярослав М.", "Ярослав А."]
     return render_template('students.html', title='Список всех студентов',
                            names=names)
+
+@app.route('/test_super')
+def test_super():
+    author_name = "Mikhail"
+    return render_template('test_super.html', author_name=author_name)
 
 if __name__ == '__main__':
     app.run()
